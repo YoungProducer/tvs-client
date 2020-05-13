31a17639640ac1e694922f2ae3a608031c33643c
YoungProducer <sashabezrukovownmail@gmail.com>
2020-05-13 10:51:51 +0300

    feat: now Editor components displays only if user are joined.

--- src/components/Room/index.tsx
+++ src/components/Room/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useStore } from 'effector-react';
 
+import { $joined } from '../RoomsDashboard/Join/model';
 import { Join } from '../RoomsDashboard/Join';
 import { Editor } from './Editor';
 import { changedRoomId } from './model';
 
 const Room = () => {
     const params = useParams();
+    const joined = useStore($joined);
 
     React.useEffect(() => {
         changedRoomId((params as any).id);
@@ -15,7 +18,7 @@ const Room = () => {
     return (
         <div>
             <Join />
-            <Editor />
+            {joined && <Editor />}
         </div>
     );
 };
