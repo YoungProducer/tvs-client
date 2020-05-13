4a62386c32ec6b513b12277b2452d7cfee2206d2
YoungProducer <sashabezrukovownmail@gmail.com>
2020-05-13 11:35:50 +0300

    feat: added ability to go to room via clicking on room name in RoomsList.

--- src/components/RoomsDashboard/RoomsList/index.tsx
+++ src/components/RoomsDashboard/RoomsList/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { useList, useStore } from 'effector-react';
 
 import { $store } from '../model';
@@ -7,6 +7,12 @@ import { $expandedRoom, changedExpandedRoom } from './model';
 import styles from './styles.module.css';
 
 const RoomsList = () => {
+    const history = useHistory();
+
+    const handlePushHistory = (path: string) => {
+        history.push(path);
+    };
+
     const rooms = useList($store, (room, index) => {
         const expandedRoomIndex = useStore($expandedRoom);
         const expanded = expandedRoomIndex === index;
@@ -15,7 +21,11 @@ const RoomsList = () => {
 
         return (
             <li key={index} className={styles.room}>
-                <p>{room.name}</p>
+                <p
+                    onClick={() => handlePushHistory(room.link)}
+                >
+                    {room.name}
+                </p>
                 <button onClick={handleOnClick}>Show more</button>
                 {expanded && <p>Link: <Link to={room.link}>{room.fullLink}</Link></p>}
             </li>
